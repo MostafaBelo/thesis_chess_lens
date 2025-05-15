b24af20ac6e79fef8d18fd740e5bb018450360f4
G4 <mostafaelfaggal@gmail.com>
2025-05-15 21:17:39 +0300

    configurable main_yolo

--- BoardDetector Yolo/main_yolo.py
+++ BoardDetector Yolo/main_yolo.py
@@ -6,10 +6,27 @@ from matplotlib import pyplot as plt
 
 from BoardDetector_YOLO import BoardDetector_YOLO
 
-bd = BoardDetector_YOLO()
+import sys
+file_path = "../data/image_0.jpg"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+if len(sys.argv) >= 2:
+    file_path = sys.argv[1]
 
-img = torch.tensor(plt.imread("../data/image_0.jpg"))
+    if len(sys.argv) >= 3:
+        device = sys.argv[2]
+
+bd = BoardDetector_YOLO()
+bd.model.to(device)
+print(bd.model.device)
+
+img = torch.tensor(plt.imread(file_path))
 # img = torch.tensor(plt.imread("../data/data_manual/1741715439429.jpg"))
+
+import time
+
+start_time = time.time()
+
+# -----------
 
 bd.set_img(img)
 mask, conf = bd.predict()
@@ -27,3 +44,8 @@ CE.apply()
 M, corners, mask_fit, error = CE.fitBoard()
 
 print(error, corners)
+
+# -----------
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time}S")

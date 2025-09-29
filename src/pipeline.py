3e33fccac769f96a54b4d16f830b447b5f9e17aa
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2025-09-29 13:41:58 +0300

    single_img_pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,34 +1,13 @@
-# %%
-import numpy as np
-from matplotlib import pyplot as plt
-
-import torch
-
-import cv2
-
+import sys
 import os
-from Dataset.data import DataPath
 
-import importlib
+import numpy as np
 import ChessLens
-importlib.reload(ChessLens)
 
-# %%
-# img = ChessLens.ChessLensImage(os.path.join(DataPath, 'data_manual/1741715439429.jpg'))
-img = ChessLens.ChessLensImage(os.path.join(
-    DataPath, 'data_manual/1741715439470.jpg'))
+img_path = sys.argv[1]
+img = ChessLens.ChessLensImage(img_path)
 
-# %%
 img.detect_board()
 # img.recognize_clock()
 img.recognize_pieces()
 img.save_fen_image()
-
-# %%
-plt.figure(figsize=(10, 10))
-
-plt.subplot(1, 2, 1)
-plt.imshow(img.img.permute(1, 2, 0))
-plt.subplot(1, 2, 2)
-plt.imshow(plt.imread("out_fen.png"))
-img.fen

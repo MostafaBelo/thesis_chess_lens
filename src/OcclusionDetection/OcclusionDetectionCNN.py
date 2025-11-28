811bf62030f0ce764a2b4bac1a6ca8165e4842cf
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2025-11-28 23:15:50 +0200

    occlusion with clahe

--- src/OcclusionDetection/OcclusionDetectionCNN.py
+++ src/OcclusionDetection/OcclusionDetectionCNN.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+import numpy as np
 
 import torch
 from torch import nn
@@ -11,6 +13,17 @@ if "device" in os.environ:
     device = os.environ["device"]
 else:
     device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def apply_clahe(image):
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    l2 = clahe.apply(l)
+
+    lab2 = cv2.merge([l2, a, b])
+    return cv2.cvtColor(lab2, cv2.COLOR_LAB2BGR)
 
 
 class TinyOcclusionCNN(nn.Module):
@@ -49,8 +62,14 @@ class OcclusionDetectorCNN:
         self.img = img
 
     def is_occluded(self):
+        img = (self.img.squeeze().permute(1, 2, 0).flip(dims=(2,)).cpu().numpy()
+               * 255).astype(np.uint8)
+        img = apply_clahe(img)
+        img = torch.tensor(
+            img, dtype=torch.float32, device=device)/255
+        img = img.permute(2, 0, 1).flip(dims=(0,)).unsqueeze(0)
         with torch.no_grad():
-            out: torch.Tensor = model(self.img.to(device))
+            out: torch.Tensor = model(img)
         pred = out.softmax(dim=1)
 
         return (pred > .5)[:, 0].item(), pred[:, 0].item()

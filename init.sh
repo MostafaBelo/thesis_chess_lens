58742bdefd99ceefedb66ebb28d070a9a6593af5
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2026-01-09 16:50:41 +0200

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 python -m venv .venv
 . .venv/bin/activate
+sudo apt install -y python3-picamera2 python3-libcamera libcamera-apps
 pip install -e .
 
-pip install gdown
 mkdir weights
-gdown --folder https://drive.google.com/drive/folders/111NYTqKwKNPBgeXxntdD4WQ5FDEb6IkG -O weights
 touch .env
 echo "WEIGHTS=\"$(pwd)/weights\"" > .env
+pip install gdown
+gdown --folder https://drive.google.com/drive/folders/111NYTqKwKNPBgeXxntdD4WQ5FDEb6IkG -O weights
